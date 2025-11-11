b3d2277d3dad3fda1f5c0b8980b3b85c01992b7b
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-11-11 13:30:24 +0100

    Refactor question qusetion and choice models

--- backend/app/models/question.py
+++ backend/app/models/question.py
@@ -1,11 +1,19 @@
 import uuid
 
 from sqlmodel import SQLModel, Field, Relationship
+from enum import Enum
+
+class answer_type(str, Enum):
+    open = "open"
+    close = "close"
+    multiple = "multiple"
+    
 
 class QuestionBase(SQLModel):
     content: str
     position: int
-    answer_type: str
+    answer_type: answer_type
+    survey_id: uuid.UUID = Field(foreign_key="survey.id")
 
 class QuestionCreate(QuestionBase):
     pass
@@ -13,23 +21,22 @@ class QuestionCreate(QuestionBase):
 
 class Question(QuestionBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    survey_id: uuid.UUID
 
-    answer_options: list['QuestionAnswerOption'] = Relationship(back_populates="question")
-
+    choice: list["Choice"] = Relationship(back_populates="question")
 
 
-class QuestionAnswerOptionBase(SQLModel):
-    question_id: uuid.UUID
+
+class ChoiceBase(SQLModel):
+    question_id: uuid.UUID = Field(foreign_key="question.id")
     position: int
     text: str
 
 
-class QuestionAnswerOptionCreate(QuestionAnswerOptionBase):
+class ChoiceCreate(ChoiceBase):
     question_position: int
 
 
-class QuestionAnswerOption(QuestionAnswerOptionBase, table=True):
+class Choice(ChoiceBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
 
-    question: Question = Relationship(back_populates="answer_options")
+    question: Question = Relationship(back_populates="choice")
