25eb015d0d1f13462b4ac0c0fb7d45fdd9426721
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-11-11 13:29:28 +0100

    Refactor user role field to enum

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,22 +5,44 @@ from pydantic import EmailStr
 from sqlmodel import SQLModel, Field, Relationship
 from datetime import datetime
 from .survey import Survey
+from enum import Enum
+from sqlalchemy import CheckConstraint
+
+
+class Role(str, Enum):
+    user = "user"
+    superuser = "superuser"
 
 
 class UserBase(SQLModel):
     email: EmailStr = Field(unique = True, index = True, max_length=255)
-    role: str = Field(default="user")
+
 
 class UserCreate(UserBase):
     password: str = Field(min_length=8, max_length=40)
 
+
 class UserPublic(UserBase):
     created_at: datetime
 
+
 class User(UserBase, table = True):
+    __table_args__ = (
+        CheckConstraint("role IN ('user','superuser')", name="chk_user_role"),
+    )
+    
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
     created_at: datetime
     is_active: bool = Field(default=True)
+    role: Role = Field(default=Role.user)
 
-    surveys: list["Survey"] = Relationship(back_populates="user")
+    surveys: list["Survey"] = Relationship(
+        back_populates="user",
+        sa_relationship_kwargs={"cascade": "all, delete"}
+        )
+    
+
+class UserUpdate(SQLModel):
+    email: EmailStr | None = None
+    password: str | None = None 
