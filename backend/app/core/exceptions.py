c42371099267f85b3a5f9ba81fda405e191b75b4
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-11-23 14:43:52 +0100

    Handle exceptions

--- backend/app/core/exceptions.py
+++ backend/app/core/exceptions.py
@@ -4,6 +4,22 @@ class BaseException(Exception):
         self.status_code = status_code
         super().__init__(self.message)
 
+
 class EmailAlreadyExistsException(BaseException):
     def __init__(self, message, status_code = 409):
+        super().__init__(message, status_code)
+
+
+class CouldNotCreateResource(BaseException):
+    def __init__(self, message, status_code = 404):
+        super().__init__(message, status_code)   
+
+
+class NotFoundError(BaseException):
+    def __init__(self, message, status_code = 404):
+        super().__init__(message, status_code)
+
+
+class NotOwnerError(BaseException):
+    def __init__(self, message, status_code = 403):
         super().__init__(message, status_code)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,21 @@
-from fastapi import FastAPI
-from .routes import user_router, login, survey_router
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from .routes import user_router, login, survey_router, question_router
 from .core.db import engine
 from . import models
 from sqlmodel import SQLModel
-
-app = FastAPI()
+from .core.exceptions import BaseException
+app = FastAPI(debug=True)
 
 app.include_router(user_router.router)
 app.include_router(login.router)
 app.include_router(survey_router.router)
+app.include_router(question_router.router)
 
 @app.get("/")
 def root():
     return {"message": "Hello in Survey APP"}
+
+@app.exception_handler(BaseException)
+async def application_error_handler(request: Request, e: BaseException):
+    return JSONResponse(status_code=e.status_code, content={"message": e.message})
