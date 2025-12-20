d9f8ec2893b343943c5eade8766f1c7e63282ef9
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-12-20 10:13:16 +0100

    Remove response-related models, CRUD logic, and API routes

--- backend/app/logic/crud/response_crud.py
+++
@@ -1,6 +0,0 @@
-from ...models.response import Response
-from sqlmodel import Session
-
-def submit_response(*, session: Session, response: Response) -> Response:
-    session.add(response)
-    return response
