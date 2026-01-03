601d49ac5c8f7972b34b81d782d55441f03aac3c
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2026-01-03 11:02:11 +0100

    Remove unused import

---
+++ backend/app/api/v1/user.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Depends
+from ...models.user import UserCreate, User, UserPublic, UserUpdate
+from ...core.db import get_session
+from sqlmodel import Session
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from ...core.auth import get_current_user
+from ...domain.services import user_service
+
+
+router = APIRouter (
+    prefix = "/user",
+    tags = ["user"]
+)
+
+@router.get("/all_users", response_model=list[UserPublic])
+def get_users(*, session: Session = Depends(get_session)):
+    return user_service.get_users(session=session)
+
+
+@router.get("/", response_model=UserPublic)
+def get_user(*, user: Annotated[User, Depends(get_current_user)]):
+    return user
+
+
+@router.post("/")
+def create_user(*, session: Session = Depends(get_session), user: UserCreate):
+    user_service.create_user(session=session, user_create=user)
+
+
+@router.delete("/")
+def delete_user(*, session: Session = Depends(get_session), user: Annotated[User, Depends(get_current_user)]):
+    user_service.delete_user(session=session, user=user)
+
+
+@router.patch("/update_user")
+def update_user(*, session: Session = Depends(get_session), user: Annotated[User, Depends(get_current_user)], user_update: UserUpdate):
+    user_service.update_user(session=session, user=user, user_update=user_update)
+
