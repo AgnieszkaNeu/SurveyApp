e5180f3bdd0ed2cc6b9be5aee0c9c7384159c0e1
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2026-01-06 10:33:55 +0100

    Add permission check in survey_owner_required function

--- backend/app/domain/policies.py
+++ backend/app/domain/policies.py
@@ -6,8 +6,12 @@ from ..models.user import User
 from .repositories.survey_repository import get_survey_by_id
 import uuid
 from ..core.transaction import transactional
+from ..core.exceptions import AccessDeniedError
 
 @transactional()
 def survey_owner_required(*, session: Session = Depends(get_session), user: User = Depends(get_current_user), survey_id: uuid.UUID):
     survey = get_survey_by_id(session, survey_id)
+    if survey.user_id != user.id:
+        raise AccessDeniedError(message="Not enough permissions")
+    
     return survey
