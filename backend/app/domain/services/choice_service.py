6c5d3f1c0b30b730c0addfc1f521b4cfefdbbde7
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2026-01-06 10:39:10 +0100

    Add transactional decorator

--- backend/app/domain/services/choice_service.py
+++ backend/app/domain/services/choice_service.py
@@ -1,9 +1,11 @@
+from ...core.transaction import transactional
 from ...models.choice import Choice, ChoiceCreate
 from ..repositories import choice_repository
 from sqlmodel import Session
 
 import uuid
 
+@transactional()
 def create_choices(session: Session, choices_create: list[ChoiceCreate], question_id: uuid.UUID):
     choices = []
     for choice_create in choices_create:
