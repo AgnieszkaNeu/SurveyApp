14323771880d3e625a30ad8762827cd88e675aa0
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2026-01-06 10:38:01 +0100

    Rename NotOwnerError to AccessDeniedError

--- backend/app/core/exceptions.py
+++ backend/app/core/exceptions.py
@@ -20,7 +20,7 @@ class NotFoundError(BaseException):
         super().__init__(message, status_code)
 
 
-class NotOwnerError(BaseException):
+class AccessDeniedError(BaseException):
     def __init__(self, message, status_code = 403):
         super().__init__(message, status_code)
 
